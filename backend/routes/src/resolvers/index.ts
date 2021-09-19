a46400a130c09fa967bad0e24ba690f1afb91d99
Aaron Pham <Aaronpham0103@gmail.com>
2021-09-19 11:30:32 +0700

    chore(backend): communications with GraphQL (#2)
    Co-authored-by: Ethan Orlander <ethanorlander@gmail.com>

--- backend/routes/src/resolvers/index.ts
+++ backend/routes/src/resolvers/index.ts
@@ -1,8 +1,20 @@
 import { QueryResolvers } from "../generated/graphql";
+import axios from "axios";
 
 const Query: QueryResolvers = {
   route: async (_, { id }, { prisma }) =>
-    prisma.route.findUnique({ where: { id } })
+    prisma.route.findUnique({ where: { id } }).then(route => ({
+      id: route.id,
+      waypoints: []
+    })),
+  createRoute: async (_, { data }) => {
+    const res = await axios.post("http://localhost:5000/api/saferoutes", data)
+    console.log(res.data);
+    return {
+      id: "gibberish",
+      waypoints: res.data.res.map(cord => ({lat: cord[0], lng:cord[1]}))
+    }
+  }
 };
 
 export default { Query };
--- backend/routes/src/schema/index.ts
+++ backend/routes/src/schema/index.ts
@@ -6,10 +6,22 @@ export default gql`
   # This primary key will be used as a unique reference for all implementing services
   type Route @key(fields: "id") {
     id: ID!
+    waypoints: [Coordinate!]!
+  }
+  
+  type Coordinate {
+    lat: Float!
+    lng: Float!
   }
 
   type Query {
     route(id: ID): Route
+    createRoute(data: CreateRouteInput): Route
+  }
+
+  input CreateRouteInput {
+    origin: String!
+    dest: String!
   }
 
 `;
